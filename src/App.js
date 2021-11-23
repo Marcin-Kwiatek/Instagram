5dc402e3685623b66b21eba0a622cc0eb5f65762
Marcin <kwiatekm4@gmail.com>
2021-11-23 18:05:13 +0100

    add react routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import React,{Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import SignUp from './components/SignUp';
 import LoginPage from './components/LoginPage';
+
 
 class App extends Component {
   render(){
   return (
-    <>
-      <LoginPage></LoginPage>
-    </>
+    <Router>    
+      <Switch>
+          <Route exact path='/'>
+              <LoginPage></LoginPage>
+          </Route>
+          <Route exact path='/SignUp'>
+              <SignUp></SignUp>
+          </Route>
+      </Switch>
+  </Router>
   )
   }
 }
