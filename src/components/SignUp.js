03fd8c73aab33b0440403329a88bd9e5b77382e2
Marcin <kwiatekm4@gmail.com>
2021-11-24 15:06:51 +0100

    styling the sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,7 @@
 import './SignUp.css';
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
+
 
 class SignUp extends Component {
     state = {
@@ -9,23 +11,27 @@ class SignUp extends Component {
     }
     SignUp = () => {
         console.log(this.state.login, this.state.password)
-        if(this.state.login === "" || this.state.password === ""){
-            this.setState({err: 'Nie poprawna wartość login lub hasło'})
+        if (this.state.login === "" || this.state.password === "") {
+            this.setState({ err: 'Nie poprawna wartość login lub hasło' })
         }
     }
     changeLogin = (e) => {
-        this.setState({login: e.target.value})
+        this.setState({ login: e.target.value })
     }
     changePassword = (e) => {
-        this.setState({password: e.target.value})
+        this.setState({ password: e.target.value })
     }
     render() {
         return (
             <div className="container">
-                <input placeholder="Login" onChange={this.changeLogin}></input>
-                <input placeholder="Hasło" onChange={this.changePassword}></input>
-                <button onClick={this.SignUp}>Sign Up</button>
-                <div>{this.state.err}</div>
+                <div className="logo">
+                    <img src={require("../img/logo.png").default}></img>
+                </div>
+                <input placeholder="Login" className="input" onChange={this.changeLogin}></input>
+                <input placeholder="Hasło" className="input" onChange={this.changePassword}></input>
+                <button className="submitButton" onClick={this.SignUp}>Sign Up</button>
+                <div className="err">{this.state.err}</div>
+                <div className="signUpText">Masz już konto? <Link className='signUpLink' to='/'>Zaloguj się</Link></div>
             </div>
         )
     }
