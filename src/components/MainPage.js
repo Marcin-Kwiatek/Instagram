15518a2d88ced8dcc81689eada20b294a475b82a
Marcin <kwiatekm4@gmail.com>
2021-12-10 13:40:29 +0100

    adding a form for adding a post

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,63 +1,88 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { AiFillHome } from "react-icons/ai";
-import { BsFillPersonFill } from "react-icons/bs";
-import {Link} from "react-router-dom";
+import { BsFillPersonFill, BsPlusSquare } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import './MainPage.css';
 
 
 class MainPage extends Component {
   state = {
-    searchUsers: []
+    searchUsers: [],
+    visibiltyAddPost: false,
+    addPostText: ''
   }
   changeSearchUser = (e) => {
     this.searchUser(e.target.value)
-}
-resetSearchUser = () => {
-  this.setState({searchUsers: []})
-}
+  }
+  changeAddPostText = (e) => {
+    this.setState({addPostText: e.target.value})
+  }
+  resetSearchUser = () => {
+    this.setState({ searchUsers: [] })
+  }
+  addPost = () => {
+    this.setState({ visibiltyAddPost: true })
+  }
+  cancelAddPost = () => {
+    this.setState({ visibiltyAddPost: false })
+  }
+  approvalOfAddingAPost = () => {
+    console.log(this.state.addPostText)
+  }
+
   searchUser = async (value) => {
     let response = await fetch(`http://localhost:5000/searchUser`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ login: value})
-            })
-            if (response.status === 404) {
-            } else {
-              let responseJson = response.json()
-              let result = await responseJson 
-              this.setState({searchUsers: result.users})
-            }
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ login: value })
+    })
+    if (response.status === 404) {
+    } else {
+      let responseJson = response.json()
+      let result = await responseJson
+      this.setState({ searchUsers: result.users })
+    }
   }
-  componentDidMount(){
+  componentDidMount() {
     fetch(`http://localhost:5000/currentUser/posts`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'authorization': localStorage.getItem('accessToken')
-                    },
-                })
-         }
-  render(){
-  return (
-    <div onClick={this.resetSearchUser}>
-      <div className="logo">
-        <div className="logoContainer">
-          <div className="imgLogo">
-            <Link to='/'><img src={require("../img/logo.png").default}></img></Link>
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'authorization': localStorage.getItem('accessToken')
+      },
+    })
+  }
+  render() {
+    return (
+      <div onClick={this.resetSearchUser}>
+        <div className="logo">
+          <div className="logoContainer">
+            <div className="imgLogo">
+              <Link to='/'><img src={require("../img/logo.png").default}></img></Link>
+            </div>
+            <input type='text' placeholder="Szukaj" className="searchUserInput" onChange={this.changeSearchUser}></input>
+            <div className="icons">
+              <Link className='iconLink' to='/'><div className="oneIcon"><AiFillHome></AiFillHome></div></Link>
+              <div className='oneIcon' onClick={this.addPost}><BsPlusSquare ></BsPlusSquare></div>
+              <Link className='iconLink' to='/myProfile'><div className="oneIcon"><BsFillPersonFill></BsFillPersonFill></div></Link>
+            </div>
           </div>
-          <input type='text' placeholder="Szukaj" className="searchUserInput" onChange={this.changeSearchUser}></input>
-          <div className="icons">
-          <Link className='iconLink' to='/'><div className="oneIcon"><AiFillHome></AiFillHome></div></Link>
-          <Link className='iconLink' to='/myProfile'><div className="oneIcon"><BsFillPersonFill></BsFillPersonFill></div></Link>
-          </div>
+          <div className='proptedUsersContainer'>{this.state.searchUsers.slice(0, 5).map(user => <div className="proptedUsers" key={user.id}>{user.login}</div>)}</div>
         </div>
-        <div className='proptedUsersContainer'>{this.state.searchUsers.slice(0,5).map(user => <div className="proptedUsers" key={user.id}>{user.login}</div>)}</div>
+        {this.state.visibiltyAddPost &&
+          <div className='addPostContainer'>
+            <div className='addText' ><input onChange={this.changeAddPostText} type='text' placeholder='enter your post content'></input></div>
+            <button onClick={this.cancelAddPost}>Cancel</button>
+            <button onClick={this.approvalOfAddingAPost}>Add Post</button>
+          </div>
+        }
+
+        <div className="posts"></div>
       </div>
-      <div className="posts"></div>
-  </div>  
-  )}
+    )
+  }
 }
 
 export default MainPage;
