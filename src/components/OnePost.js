6380736eae8a14e55b1c5ddb3b0a2d50e9411df7
Marcin <kwiatekm4@gmail.com>
2022-01-18 15:48:18 +0100

    move some watched posts elements to the new one post component

---
+++ src/components/OnePost.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { AiFillHeart, AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
+import './WatchedUsersPosts.css';
+
+
+class OnePost extends Component {
+    state = {
+        visibilityLikeIcon: 'inline',
+        visibilityUnlikeIcon: 'none'
+    }
+    unlikePhoto = () => {
+        this.setState({ visibilityLikeIcon: 'inline' })
+        this.setState({ visibilityUnlikeIcon: 'none' })
+    }
+    likePhoto = (postId) => {
+        this.setState({ visibilityLikeIcon: 'none' })
+        this.setState({ visibilityUnlikeIcon: 'inline' })
+    }
+    render() {
+        return (
+            <>
+                <div className={'watchedUsersPost'}>
+                    <div className='watchedPostNick'>{this.props.login}</div>
+                    <img className='watchedPostImage' src={`http://localhost:5000/${this.props.imageUrl}`} />
+                    <div className='watchedPostIcons'>
+                        <div className='oneWatchedPostIcon' onClick={this.unlikePhoto}
+                            style={{ display: this.state.visibilityUnlikeIcon, color: 'red' }}><AiFillHeart></AiFillHeart></div>
+                        <div className='oneWatchedPostIcon' onClick={() => this.likePhoto(this.props.id)}
+                            style={{ display: this.state.visibilityLikeIcon }}><AiOutlineHeart></AiOutlineHeart></div>
+                        <div className='oneWatchedPostIcon'><AiOutlineMessage></AiOutlineMessage></div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default OnePost;
--- src/components/WatchedUsersPosts.js
+++ src/components/WatchedUsersPosts.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import './WatchedUsersPosts.css';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { AiFillHeart, AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
-
-
-
+import OnePost from './OnePost';
 
 class WatchedUsersPosts extends Component {
     state = {
-        posts: [],
-        visibilityLikeIcon: 'inline',
-        visibilityUnlikeIcon: 'none'
+        posts: []
     }
     showMoreObservedPosts = () => {
         fetch(`http://localhost:5000/currentUser/observedUsers/posts?offset=${this.state.posts.length}&limit=10`, {
@@ -28,38 +23,21 @@ class WatchedUsersPosts extends Component {
                 }
             })
     }
-    unlikePhoto = () => {
-        this.setState({visibilityLikeIcon: 'inline'})
-        this.setState({visibilityUnlikeIcon: 'none'})
-    }
-    likePhoto = () => {
-        this.setState({visibilityLikeIcon: 'none'})
-        this.setState({visibilityUnlikeIcon: 'inline'})
-    }
     componentDidMount() {
         this.showMoreObservedPosts()
     }
     render() {
         return (
             <div className={'containerWatchedUsersPosts'}>
-
                 <InfiniteScroll
                     dataLength={this.state.posts.length}
                     next={this.showMoreObservedPosts}
                     hasMore={true}
                 >
                     {this.state.posts.map(post =>
-                        <div className={'watchedUsersPost'} key={post.id}>
-                            <div className='watchedPostNick'>{post.login}</div>
-                            <img className='watchedPostImage' src={`http://localhost:5000/${post.imageUrl}`} />
-                            <div className='watchedPostIcons'>
-                                <div className='oneWatchedPostIcon' onClick={this.unlikePhoto} 
-                                style={{ display: this.state.visibilityUnlikeIcon, color:'red'}}><AiFillHeart></AiFillHeart></div>
-                                <div className='oneWatchedPostIcon' onClick={this.likePhoto} 
-                                style={{ display: this.state.visibilityLikeIcon }}><AiOutlineHeart></AiOutlineHeart></div>
-                                <div className='oneWatchedPostIcon'><AiOutlineMessage></AiOutlineMessage></div>
-                            </div>
-                        </div>
+                        <OnePost key={post.id} imageUrl={post.imageUrl} login={post.login}
+                            id={post.id}>
+                        </OnePost>
                     )}
                 </InfiniteScroll>
             </div>
