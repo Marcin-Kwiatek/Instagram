926806eda8b56566f3bb719fb15bc9df7cda3c3b
Marcin <kwiatekm4@gmail.com>
2022-02-13 11:30:25 +0100

    change OneWatchedPost component from class to function

--- src/components/OneWatchedPost.js
+++ src/components/OneWatchedPost.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AiFillHeart, AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
 import './WatchedUsersPosts.css';
 import generateId from '../utils/generateId';
@@ -7,18 +7,18 @@ import EmojiInput from './EmojiInput'
 
 
 
-class OneWatchedPost extends Component {
-    state = {
-        visibilityLikeIcon: '',
-        visibilityUnlikeIcon: '',
-        newComment: '',
-        comments: [],
-        modalPostComments: [],
-        visibilityPostModal: 'none',
-    }
-    unlikePhoto = (postId) => {
-        this.setState({ visibilityLikeIcon: 'inline' })
-        this.setState({ visibilityUnlikeIcon: 'none' })
+function OneWatchedPost(props) {
+
+    const [visibilityLikeIcon, setVisibilityLikeIcon] = useState('');
+    const [visibilityUnlikeIcon, setVisibilityUnlikeIcon] = useState('');
+    const [newComment, setNewComment] = useState('');
+    const [comments, setComments] = useState([]);
+    const [modalPostComments, setModalPostComments] = useState([]);
+    const [visibilityPostModal, setVisibilityPostModal] = useState('none');
+
+    const unlikePhoto = (postId) => {
+        setVisibilityLikeIcon('inline')
+        setVisibilityUnlikeIcon('none')
         fetch(`http://localhost:5000/likes?id=${postId}`, {
             method: 'DELETE',
             headers: {
@@ -27,9 +27,9 @@ class OneWatchedPost extends Component {
         })
             .then(function (response) { return response.json() })
     }
-    likePhoto = (postId) => {
-        this.setState({ visibilityLikeIcon: 'none' })
-        this.setState({ visibilityUnlikeIcon: 'inline' })
+    const likePhoto = (postId) => {
+        setVisibilityLikeIcon('none')
+        setVisibilityUnlikeIcon('inline')
         fetch(`http://localhost:5000/likes`, {
             method: 'POST',
             headers: {
@@ -40,135 +40,134 @@ class OneWatchedPost extends Component {
         })
             .catch((err) => { console.error(err) })
     }
-    async componentDidMount() {
+    useEffect(async () => {
         try {
-            let response = await fetch(`http://localhost:5000/likes?likedPostId=${this.props.id}`, {
+            let response = await fetch(`http://localhost:5000/likes?likedPostId=${props.id}`, {
                 headers: {
                     'Content-Type': 'application/json',
                     'authorization': localStorage.getItem("accessToken")
                 },
             })
             if (response.status === 404) {
-                this.setState({ visibilityLikeIcon: 'inline' })
-                this.setState({ visibilityUnlikeIcon: 'none' })
+                setVisibilityLikeIcon('inline')
+                setVisibilityUnlikeIcon('none')
             } else {
-                this.setState({ visibilityLikeIcon: 'none' })
-                this.setState({ visibilityUnlikeIcon: 'inline' })
+                setVisibilityLikeIcon('none')
+                setVisibilityUnlikeIcon('inline')
             }
         }
         catch (error) {
             console.error(error)
         }
-        fetch(`http://localhost:5000/comments?id=${this.props.id}&limit=3`, {})
+        fetch(`http://localhost:5000/comments?id=${props.id}&limit=3`, {})
             .then(function (posts) { return (posts.json()) })
             .then((result) => {
                 if (result.data === null) {
-                    this.setState({ comments: [] })
+                    setComments([])
                 }
                 else {
-                    this.setState({ comments: result.data })
+                    setComments(result.data)
                 }
             })
+    }, [])
+    const changeNewComment = (inputValue) => {
+        setNewComment(inputValue)
     }
-    changeNewComment = (inputValue) => {
-        this.setState({ newComment: inputValue })
-    }
-    addComment = (postId) => {
-        if (this.state.newComment !== "") {
+    const addComment = (postId) => {
+        if (newComment !== "") {
             fetch(`http://localhost:5000/comment`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                     'authorization': localStorage.getItem("accessToken")
                 },
-                body: JSON.stringify({ postId: postId, commentContent: this.state.newComment, id: generateId(), date: currentDate() })
+                body: JSON.stringify({ postId: postId, commentContent: newComment, id: generateId(), date: currentDate() })
             }).then(() => {
-                this.setState({ newComment: '' })
+                setNewComment('')
             })
                 .catch((err) => { console.error(err) })
         }
     }
-    modalPostShow = () => {
-        fetch(`http://localhost:5000/comments?id=${this.props.id}&limit=100`, {})
+    const modalPostShow = () => {
+        fetch(`http://localhost:5000/comments?id=${props.id}&limit=100`, {})
             .then(function (posts) { return (posts.json()) })
             .then((result) => {
                 if (result.data === null) {
-                    this.setState({ modalPostComments: [] })
-                    this.setState({ visibilityPostModal: 'inline' })
+                    setModalPostComments([])
+                    setVisibilityPostModal('inline')
                 }
                 else {
-                    this.setState({ modalPostComments: result.data })
-                    this.setState({ visibilityPostModal: 'inline' })
+                    setModalPostComments(result.data)
+                    setVisibilityPostModal('inline')
                 }
             })
     }
-    render() {
-        return (
-            <>
-                <div className={'watchedUsersPost'}>
-                    <div className='watchedPostNick'>{this.props.login}</div>
-                    <img className='watchedPostImage' src={`http://localhost:5000/${this.props.imageUrl}`} />
-                    <div className='watchedPostIcons'>
-                        <div className='oneWatchedPostIcon' onClick={() => this.unlikePhoto(this.props.id)}
-                            style={{ display: this.state.visibilityUnlikeIcon, color: 'red' }}><AiFillHeart></AiFillHeart></div>
-                        <div className='oneWatchedPostIcon' onClick={() => this.likePhoto(this.props.id)}
-                            style={{ display: this.state.visibilityLikeIcon }}><AiOutlineHeart></AiOutlineHeart></div>
-                        <div className='oneWatchedPostIcon'><AiOutlineMessage></AiOutlineMessage></div>
-                    </div>
-                    <div className='likesText'>{this.props.likesNr} users like this</div>
-                    <div className='viewAllCommentsText' onClick={this.modalPostShow}>View all comments</div>
-                    <div className='obscureBackground' style={{ display: this.state.visibilityPostModal }}></div>
-                    <div className='modalPost' style={{ display: this.state.visibilityPostModal }}>
-                        <img className='modalImage' src={`http://localhost:5000/${this.props.imageUrl}`}></img>
-                        <div className='modalCommentContainer'>
-                            <div className='modalPostNick'>{this.props.login}</div>
-                            <div className='comments' id='modalPostComments'>
-                                {this.state.modalPostComments.map(comment =>
-                                    <div key={comment.id} className='comment' id='modalPostComment'>
-                                        <div className='commentLogin'>{comment.login}</div>
-                                        <div className='commentContent'>{comment.commentContent}</div>
-                                    </div>
-                                )}
+    return (
+        <>
+            <div className={'watchedUsersPost'}>
+                <div className='watchedPostNick'>{props.login}</div>
+                <img className='watchedPostImage' src={`http://localhost:5000/${props.imageUrl}`} />
+                <div className='watchedPostIcons'>
+                    <div className='oneWatchedPostIcon' onClick={() => unlikePhoto(props.id)}
+                        style={{ display: visibilityUnlikeIcon, color: 'red' }}><AiFillHeart></AiFillHeart></div>
+                    <div className='oneWatchedPostIcon' onClick={() => likePhoto(props.id)}
+                        style={{ display: visibilityLikeIcon }}><AiOutlineHeart></AiOutlineHeart></div>
+                    <div className='oneWatchedPostIcon'><AiOutlineMessage></AiOutlineMessage></div>
+                </div>
+                <div className='likesText'>{props.likesNr} users like this</div>
+                <div className='viewAllCommentsText' onClick={modalPostShow}>View all comments</div>
+                <div className='obscureBackground' style={{ display: visibilityPostModal }}></div>
+                <div className='modalPost' style={{ display: visibilityPostModal }}>
+                    <img className='modalImage' src={`http://localhost:5000/${props.imageUrl}`}></img>
+                    <div className='modalCommentContainer'>
+                        <div className='modalPostNick'>{props.login}</div>
+                        <div className='comments' id='modalPostComments'>
+                            {modalPostComments.map(comment =>
+                                <div key={comment.id} className='comment' id='modalPostComment'>
+                                    <div className='commentLogin'>{comment.login}</div>
+                                    <div className='commentContent'>{comment.commentContent}</div>
+                                </div>
+                            )}
+                        </div>
+                        <div className='lowerPartOfModal'>
+                            <div className='watchedPostIcons' id='modalWatchedPostIcons'>
+                                <div className='oneWatchedPostIcon' onClick={() => unlikePhoto(props.id)}
+                                    style={{ display: visibilityUnlikeIcon, color: 'red' }}><AiFillHeart></AiFillHeart></div>
+                                <div className='oneWatchedPostIcon' onClick={() => likePhoto(props.id)}
+                                    style={{ display: visibilityLikeIcon }}><AiOutlineHeart></AiOutlineHeart></div>
+                                <div className='oneWatchedPostIcon'><AiOutlineMessage></AiOutlineMessage></div>
                             </div>
-                            <div className='lowerPartOfModal'>
-                                <div className='watchedPostIcons' id='modalWatchedPostIcons'>
-                                    <div className='oneWatchedPostIcon' onClick={() => this.unlikePhoto(this.props.id)}
-                                        style={{ display: this.state.visibilityUnlikeIcon, color: 'red' }}><AiFillHeart></AiFillHeart></div>
-                                    <div className='oneWatchedPostIcon' onClick={() => this.likePhoto(this.props.id)}
-                                        style={{ display: this.state.visibilityLikeIcon }}><AiOutlineHeart></AiOutlineHeart></div>
-                                    <div className='oneWatchedPostIcon'><AiOutlineMessage></AiOutlineMessage></div>
+                            <div className='likesText' id='modalLikesText'>{props.likesNr} users like this</div>
+                            <div className='addComment' id='modalPostAddComment'>
+                                <div className='addCommentInput' id='addCommentModalInput'>
+                                    <EmojiInput onChange={changeNewComment} inputValue={newComment}></EmojiInput>
                                 </div>
-                                <div className='likesText' id='modalLikesText'>{this.props.likesNr} users like this</div>
-                                <div className='addComment' id='modalPostAddComment'>
-                                    <div className='addCommentInput' id='addCommentModalInput'>
-                                        <EmojiInput onChange={this.changeNewComment} inputValue={this.state.newComment}></EmojiInput>
-                                    </div>
-                                    <button className='addCommentButton' onClick={() => this.addComment(this.props.id)}>Publish</button>
-                                </div>
+                                <button className='addCommentButton' onClick={() => addComment(props.id)}>Publish</button>
                             </div>
                         </div>
-                    </div>
-                    <div className='hideModalPost' style={{ display: this.state.visibilityPostModal }}
-                        onClick={() => this.setState({ visibilityPostModal: 'none' })}>x
-                    </div>
-                    <div className='comments'>
-                        {this.state.comments.map(comment =>
-                            <div className='comment' key={comment.id}>
-                                <div className='commentLogin'>{comment.login}</div>
-                                <div className='commentContent'>{comment.commentContent}</div>
-                            </div>
-                        )}
-                    </div>
-                    <div className='addComment'>
-                        <div className='addCommentInput'>
-                            <EmojiInput onChange={this.changeNewComment} inputValue={this.state.newComment}></EmojiInput>
-                        </div>
-                        <button className='addCommentButton' onClick={() => this.addComment(this.props.id)}>Publish</button>
                     </div>
                 </div>
-            </>
-        )
-    }
+                <div className='hideModalPost' style={{ display: visibilityPostModal }}
+                    onClick={() => setVisibilityPostModal('none')}>x
+                </div>
+                <div className='comments'>
+                    {comments.map(comment =>
+                        <div className='comment' key={comment.id}>
+                            <div className='commentLogin'>{comment.login}</div>
+                            <div className='commentContent'>{comment.commentContent}</div>
+                        </div>
+                    )}
+                </div>
+                <div className='addComment'>
+                    <div className='addCommentInput'>
+                        <EmojiInput onChange={changeNewComment} inputValue={newComment}></EmojiInput>
+                    </div>
+                    <button className='addCommentButton' onClick={() => addComment(props.id)}>Publish</button>
+                </div>
+            </div>
+        </>
+    )
+
 }
 
 export default OneWatchedPost;
