a8910fe7829b8957d9623e4bfacd6cfd9fcfe053
Marcin <kwiatekm4@gmail.com>
2022-02-13 10:20:02 +0100

    fix adding emoji to database

--- server/dbService.js
+++ server/dbService.js
@@ -9,6 +9,7 @@ const connection = mysql.createConnection({
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     port: process.env.DB_PORT,
+    charset : 'utf8mb4'
 })
 
 connection.connect((err) => {
