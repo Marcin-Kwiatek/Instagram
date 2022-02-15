e31d149898964ff4392ecc4413196657dd3dc590
Marcin <kwiatekm4@gmail.com>
2022-02-15 14:58:26 +0100

    change component LoginPage from class to function

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,22 +1,19 @@
 import './FormFields.css';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
-function EnhanceLoginPageWithHistory(props){
-    const history = useHistory()
-    return <LoginPage history={history} {...props}></LoginPage>
-}
 
-class LoginPage extends Component {
-    state = {
-        login: '',
-        password: '',
-        err: ''
-    }
-    Login = () => {
-        if (this.state.login === "" || this.state.password === "") {
-            this.setState({ err: 'Nie poprawna wartość login lub hasło' })
+function LoginPage() {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [err, setErr] = useState('');
+
+    const history = useHistory()
+
+    const Login = () => {
+        if (login === "" || password === "") {
+            setErr('Nie poprawna wartość login lub hasło')
         }
         else {
             fetch(`http://localhost:5000/signIn`, {
@@ -24,42 +21,41 @@ class LoginPage extends Component {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ login: this.state.login, password: this.state.password })
+                body: JSON.stringify({ login: login, password: password })
             })
                 .then((response) => {
                     if (response.status === 404) {
-                        this.setState({ err: 'Nie znaleziono użytkownika o podanym loginie lub haśle' })
+                        setErr('Nie znaleziono użytkownika o podanym loginie lub haśle')
                     } else {
                         response.json().then(data => {
                             localStorage.setItem('accessToken', data.accessToken);
                             localStorage.setItem('currentUserId', data.userId);
-                            this.props.history.push("/");
+                            history.push("/");
                         })
                     }
                 })
         }
     }
-    changeLogin = (e) => {
-        this.setState({ login: e.target.value })
+    const changeLogin = (e) => {
+        setLogin(e.target.value)
     }
-    changePassword = (e) => {
-        this.setState({ password: e.target.value })
+    const changePassword = (e) => {
+        setPassword(e.target.value)
     }
-    render() {
-        return (
-            <div className="container">
-                <div className="logo">
-                    <img src={require("../img/logo.png").default}></img>
-                </div>
-                <input placeholder="Login" className="input" maxLength="20" type="text" onChange={this.changeLogin}></input>
-                <input placeholder="Hasło" className="input" maxLength="20" type="password" onChange={this.changePassword}></input>
-                <button className="submitButton" onClick={this.Login}>Zaloguj się</button>
-                <div className="err">{this.state.err}</div>
-                <div className="text">Nie masz konta? <Link className='link' to='/SignUp'>Zarejestruj się</Link>
-                </div>
+    return (
+        <div className="container">
+            <div className="logo">
+                <img src={require("../img/logo.png").default}></img>
             </div>
-        )
-    }
+            <input placeholder="Login" className="input" maxLength="20" type="text" onChange={changeLogin}></input>
+            <input placeholder="Hasło" className="input" maxLength="20" type="password" onChange={changePassword}></input>
+            <button className="submitButton" onClick={Login}>Zaloguj się</button>
+            <div className="err">{err}</div>
+            <div className="text">Nie masz konta? <Link className='link' to='/SignUp'>Zarejestruj się</Link>
+            </div>
+        </div>
+    )
+
 }
 
-export default EnhanceLoginPageWithHistory;
+export default LoginPage;
