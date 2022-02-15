c48ad3bfe0fc03aa1f01727c19b0e2f042de070c
Marcin <kwiatekm4@gmail.com>
2022-02-15 16:18:56 +0100

    change component SignUp from class to function

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,26 +1,20 @@
 import './FormFields.css';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import generateId from '../utils/generateId';
 
-function EnhanceSignUpWithHistory(props){
+function SignUp() {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [err, setErr] = useState('');
+
     const history = useHistory()
-    return <SignUp history={history} {...props}></SignUp>
-}
-class SignUp extends Component {
-    constructor(props){
-        super(props)
-        this.SignUp = this.SignUp.bind(this)
-    }
-    state = {
-        login: '',
-        password: '',
-        err: ''
-    }
-    SignUp = () => {
-        if (this.state.login === "" || this.state.password === "") {
-            this.setState({ err: 'Nie poprawna wartość login lub hasło' })
+
+    const SignUp = () => {
+        if (login === "" || password === "") {
+            setErr('Nie poprawna wartość login lub hasło')
         }
         else {
             fetch(`http://localhost:5000/signUp`, {
@@ -28,46 +22,43 @@ class SignUp extends Component {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ login: this.state.login, password: this.state.password })
+                body: JSON.stringify({ login: login, password: password })
             })
                 .then((response) => {
                     if (response.status === 404) {
-                        this.setState({ err: 'Ten login jest już zajęty' })
+                        setErr('Ten login jest już zajęty')
                     } else {
                         fetch(`http://localhost:5000/user`, {
                             method: 'POST',
                             headers: {
                                 'Content-Type': 'application/json'
                             },
-                            body: JSON.stringify({ id: generateId(), login: this.state.login, password: this.state.password})
+                            body: JSON.stringify({ id: generateId(), login: login, password: password })
                         })
-                        this.props.history.push("/SignIn");
+                        history.push("/SignIn");
                     }
                 })
 
         }
     }
-    changeLogin = (e) => {
-        this.setState({ login: e.target.value })
+    const changeLogin = (e) => {
+        setLogin(e.target.value)
     }
-    changePassword = (e) => {
-        this.setState({ password: e.target.value })
+    const changePassword = (e) => {
+        setPassword(e.target.value)
     }
-    render() {
-        return (
-            <div className="container">
-                <div className="logo">
-                    <img src={require("../img/logo.png").default}></img>
-                </div>
-                <input placeholder="Login" className="input" onChange={this.changeLogin}></input>
-                <input placeholder="Hasło" className="input" onChange={this.changePassword} type="password"></input>
-                <button className="submitButton" onClick={this.SignUp}>Sign Up</button>
-                <div className="err">{this.state.err}</div>
-                <div className="text">Masz już konto? <Link className='link' to='/SignIn'>Zaloguj się</Link></div>
+    return (
+        <div className="container">
+            <div className="logo">
+                <img src={require("../img/logo.png").default}></img>
             </div>
-        )
-    }
-
+            <input placeholder="Login" className="input" onChange={changeLogin}></input>
+            <input placeholder="Hasło" className="input" onChange={changePassword} type="password"></input>
+            <button className="submitButton" onClick={SignUp}>Sign Up</button>
+            <div className="err">{err}</div>
+            <div className="text">Masz już konto? <Link className='link' to='/SignIn'>Zaloguj się</Link></div>
+        </div>
+    )
 }
 
-export default EnhanceSignUpWithHistory;
+export default SignUp;
