9712daf61ef6fa1f83b90c07ad62d2c4a47f3895
Marcin <kwiatekm4@gmail.com>
2022-02-15 15:05:41 +0100

    change component  MyIntro from class to function

--- src/components/MyIntro.js
+++ src/components/MyIntro.js
@@ -1,30 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './PersonIntro.css';
 
 
-class MyIntro extends Component {
-    state = {
-        nickName: []
-    }
+function MyIntro() {
+    const [nickName, setNickName] = useState([]);
 
-    async componentDidMount() {
+    useEffect(async () => {
         const userId = localStorage.getItem("currentUserId")
         fetch(`http://localhost:5000/personIntro?id=${userId}`, {})
             .then(function (response) { return response.json() })
             .then((data) => {
                 let nick = data.data
-                this.setState({ nickName: nick })
+                setNickName(nick)
             })
             .catch((error) => console.error(error))
-    }
-    render() {
-        return (
-            <div className='introContainer'>
-                {this.state.nickName}
-            </div>
-        )
-    }
-
+    }, [])
+    return (
+        <div className='introContainer'>
+            {this.state.nickName}
+        </div>
+    )
 }
 
 export default MyIntro;
